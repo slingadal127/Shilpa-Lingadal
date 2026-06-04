e48d3a35a497cbc48736f9d867001dacb885f9df
Shilpa Lingdal <lingadal.s@northeastern.edu>
2026-06-04 04:46:45 -0400

    Set basePath for GitHub Pages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/sde-portfolio" : "",
   pageExtensions: ["ts", "tsx", "mdx"],
   images: {
     unoptimized: true, // required for static export
